136bad4a06c366e2ed04cae9ebf4230b375f76b6
rileydollar8@gmail.com <ryan.anzaye@student.moringaschool.com>
2024-04-24 22:36:15 +0300

    work

---
+++ src/components/ArticleList.js
@@ -0,0 +1,15 @@
+// ArticleList.js
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map((post, index) => (
+        <Article key={index} post={post} />
+      ))}
+    </main>
+  )
+}
+
+export default ArticleList;
